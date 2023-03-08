7c1570668337276f371d3974ea4c0d13041d821c
dai <83387146+dai2024@users.noreply.github.com>
2023-03-08 05:21:34 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,25 +2,39 @@
 #include <pthread.h>
 #include "sub.h"
 
-void *thread_function(void *arg) {
-  printf("Thread running\n");
-  int result = sub();
-  printf("Result from thread: %d\n", result);
-  pthread_exit(NULL);
+pthread_mutex_t lock;
+
+void *thread_func(void *arg) {
+    //
+    pthread_mutex_lock(&lock);
+    int result = sub1();
+    pthread_mutex_unlock(&lock);
+    return (void*)result;
 }
 
-int main() {
-  pthread_t my_thread;
-  printf("Creating thread\n");
-  if (pthread_create(&my_thread, NULL, thread_function, NULL)) {
-    printf("error creating thread.");
-    abort();
-  }
-  printf("Waiting for thread to finish\n");
-  if (pthread_join(my_thread, NULL)) {
-    printf("error joining thread.");
-    abort();
-  }
-  printf("Thread finished\n");
-  return 0;
+int main(void) {
+    pthread_t thread1, thread2;
+    int result1, result2;
+
+    //
+    pthread_mutex_init(&lock, NULL);
+
+    //
+    pthread_create(&thread1, NULL, thread_func, NULL);
+
+    //
+    pthread_create(&thread2, NULL, thread_func, NULL);
+
+    //
+    pthread_join(thread1, (void **)&result1);
+    pthread_join(thread2, (void **)&result2);
+
+    //
+    printf("Result 1: %d\n", result1);
+    printf("Result 2: %d\n", result2);
+
+    //
+    pthread_mutex_destroy(&lock);
+
+    return 0;
 }
