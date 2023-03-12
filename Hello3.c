ca148e665231ed32e7e1571b6299c662a86b845a
dai <83387146+dai2024@users.noreply.github.com>
2023-03-12 05:37:08 +0000

    Update Hello3.c

--- Hello3.c
+++ Hello3.c
@@ -1,6 +1,3 @@
-// #include <threads.h>
-// #include <stdlib.h>
-
 static int sub1(void) {
 	return sub2(); 
 	// return (0); // EXIT_SUCCESS
@@ -9,5 +6,4 @@ static int sub1(void) {
 volatile int main(void) {
 	int (*func)(void) = sub1;
 	return func();
-	// return (func() == -1) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
