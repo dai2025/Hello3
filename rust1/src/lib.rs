56d6ca05b0da0f5faf46ed979349c16dbd6a45ff
dai <83387146+dai2024@users.noreply.github.com>
2023-04-03 11:59:35 +0000

    Update lib.rs

--- rust1/src/lib.rs
+++ rust1/src/lib.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Write};
+
+#[no_mangle]
+pub extern "C" fn add(a: i32, b: i32) -> i32 {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    writeln!(handle, "Adding {} and {}", a, b).unwrap();
+    let result = a + b;
+    writeln!(handle, "Result: {}", result).unwrap();
+    result
+}
+
 #[no_mangle]
 pub extern "C" fn add_numbers(a: i32, b: i32) -> i32 {
     a + b
@@ -7,3 +19,5 @@ pub extern "C" fn add_numbers(a: i32, b: i32) -> i32 {
 pub extern "C" fn sub2() -> i32 {
     -1
 }
+
+
