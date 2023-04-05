27b50130ab1f24bcda2e3f7637723bb1373c3747
dai <83387146+dai2024@users.noreply.github.com>
2023-04-05 03:26:03 +0000

    Update lib.rs
    3

--- rust1/src/lib.rs
+++ rust1/src/lib.rs
@@ -20,4 +20,55 @@ pub extern "C" fn sub2() -> i32 {
     -1
 }
 
+/*
+use std::rc::Rc;
+use std::rc::Weak;
+use std::cell::RefCell;
+
+struct Class {
+name: String,
+gc: RefCell<Vec<Weak<String>>>,
+}
+
+//#[allow(dead_code)]
+fn println_gc(class: &Class) {
+println!("In {} class:", class.name);
+for weak_name in class.gc.borrow().iter() {
+if let Some(name) = weak_name.upgrade() {
+println!("{}", name);
+} else {
+println!("unavailable");
+}
+}
+}
+
+fn print_gc(class: &Class) {
+print!("{} ", class.name);
+for weak_name in class.gc.borrow().iter() {
+if let Some(name) = weak_name.upgrade() {
+print!("{} ", name);
+} else {
+print!("unavailable");
+}
+}
+}
+
+fn main() {
+let world = Rc::new("world!".to_string());
+let three = Rc::new("3!".to_string());
+let class : Rc<Class> = Rc::new(
+Class {
+name: String::from("Hello"),
+gc: RefCell::new(vec![]),
+}
+);
+class.gc.borrow_mut().push(Rc::downgrade(&world));
+class.gc.borrow_mut().push(Rc::downgrade(&three));
+//println!("{} {:?}", class1.name, class1);
+println!("{}", class.name);
+println_gc(&class);
+class.gc.borrow_mut().pop();
+print_gc(&class);
+}
+*/
 
